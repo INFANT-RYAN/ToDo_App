4a0c746302d8f175e015c54bec1fb8b27c784f15
Infant-Ryan <ini0k17@gmail.com>
2025-01-18 11:11:29 +0530

    chore:

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
 
 export default function App() {
-  
-
   const [tasks, setTasks] = useState([]);
   const [ntask, setNewTask] = useState("");
 
   function addTask() {
-    if (ntask.trim() !== "") {
+    if (ntask.trim() !== ""){
       setTasks([...tasks, ntask]);
       setNewTask("");
     } else alert("Enter task before add");
@@ -22,41 +20,41 @@ export default function App() {
   }
 
   return (
-    <div className="mx-auto container flex justify-center items-center md:bg-[#14213d]  md:min-h-screen">
+    <div className="mx-auto container flex justify-center items-center   ">
       <div className="md:border-2 bg-white border-[#fca311] md:w-[40%] min-h-screen min-w-full  md:h-96  rounded-lg p-5">
-      <h1 className="font-bold text-3xl text-[#14213d] text-center">To-Do</h1>
-      <div className="flex justify-center my-5 w-[100%]">
-        <input
-          type="text"
-          className="border-2 w-[80%] h-10  rounded-md  p-2"
-          value={ntask}
-          onChange={inputChange}
-        />
-        <button
-          className="  text-[#14213d] font-semibold bg-[#fca311] rounded-md h-10 w-14"
-          onClick={addTask}
-        >
-          ADD
-        </button>
+        <h1 className="font-bold text-3xl text-[#14213d] text-center">To-Do</h1>
+        <div className="flex justify-center my-5 w-[100%]">
+          <input
+            type="text"
+            className="border-2 w-[80%] h-10  rounded-md  p-2"
+            value={ntask}
+            onChange={inputChange}
+          />
+          <button
+            className="  text-[#14213d] font-semibold bg-[#fca311] rounded-md h-10 w-[20%]"
+            onClick={addTask}
+          >
+            ADD
+          </button>
+        </div>
+        <div className="flex justify-center w-[100%]">
+          <ol className="w-[90%]">
+            {tasks.map((t, index) => (
+              <li className="bg-[#fca311] p-2 rounded-md my-2 " key={index}>
+                <div className="flex justify-between">
+                  <span>{t}</span>
+                  <button
+                    className="bg-white px-1 rounded-sm text-[#14213d] font-semibold   "
+                    onClick={() => delTask(index)}
+                  >
+                    Done
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
       </div>
-      <div className="flex justify-center w-[100%]">
-        <ol className="w-[90%]">
-          {tasks.map((t, index) => (
-            <li className="bg-[#fca311] p-2 rounded-md my-2 " key={index}>
-              <div className="flex justify-between">
-                <span>{t}</span>
-                <button
-                  className="bg-white px-1 rounded-sm text-[#14213d] font-semibold   "
-                  onClick={() => delTask(index)}
-                >
-                  Done
-                </button>
-              </div>
-            </li>
-          ))}
-        </ol>
-      </div>
-    </div>
     </div>
   );
 }
