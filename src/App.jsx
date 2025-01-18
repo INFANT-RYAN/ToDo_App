80726cd1cd74fee11a92abee1ca13d758b0e3b5a
Infant-Ryan <ini0k17@gmail.com>
2025-01-18 10:46:00 +0530

    fix: responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,8 @@ export default function App() {
   }
 
   return (
-    <div className="flex justify-center items-center bg-[#14213d] min-h-screen">
-      <div className="border-2 bg-white border-[#fca311] w-[40%] h-96 rounded-lg p-5">
+    <div className="mx-auto container flex justify-center items-center md:bg-[#14213d]  md:min-h-screen">
+      <div className="border-2 bg-white border-[#fca311] md:w-[40%] min-h-screen min-w-full  md:h-96  rounded-lg p-5">
       <h1 className="font-bold text-3xl text-[#14213d] text-center">To-Do</h1>
       <div className="flex justify-center my-5 w-[100%]">
         <input
